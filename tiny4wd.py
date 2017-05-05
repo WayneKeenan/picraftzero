aa9e43f431b2f466ff92814e613f39396986537f
Wayne Keenan <WayneKeenan@users.noreply.github.com>
2017-05-05 06:08:39 +0100

    Fix typos

--- tiny4wd.py
+++ tiny4wd.py
@@ -2,7 +2,7 @@ from signal import pause
 from picraft.zero import Joystick, Wheelbase, steering_mixer
 
 # Joystick axis values are  (left/down)  -100 .. 100   (right/up)
-# Motor expect for left and right:   (full speed backwards) -100  .. 100 (full speed forwards)
+# Motor expects for left and right:   (full speed backwards) -100  .. 100 (full speed forwards)
 
 joystick= Joystick()   # find the first available controller (e.g. Rock Candy, PS3, PS4, Wii)
 tiny4wd_motors = Wheelbase(left=0, right=1)  # left/right= logical id of i2c motor (auto-detected Explorer pHAT or PiConZero)
@@ -11,6 +11,3 @@ tiny4wd_motors = Wheelbase(left=0, right=1)  # left/right= logical id of i2c mot
 tiny4wd_motors.source = steering_mixer(joystick.values)
 
 pause()
-
-
-
