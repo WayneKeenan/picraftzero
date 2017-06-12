b58ba85f94537e3a390f7944d0f4507ce40d0f0c
Wayne Keenan <wayne.keenan@gmail.com>
2017-06-12 10:25:48 +0100

    use unittest discovery

---
+++ test/test_steering.py
@@ -0,0 +1,11 @@
+﻿from unittest import TestCase
+
+from picraftzero.utils import differential_steering
+
+class SteeringMixerTest(TestCase):
+
+    def test_zero(self):
+        (speed_a, speed_b) = differential_steering(0,0)
+        self.assertEqual(speed_a, 0, "Speed A not 0")
+        self.assertEqual(speed_b, 0, "Speed B not 0")
+
