c6b132061753128851245d779c15120bf705153d
Wayne Keenan <wayne.keenan@gmail.com>
2017-06-19 05:35:24 +0100

    Release 0.2.3

--- picraftzero/version.py
+++ picraftzero/version.py
@@ -1,5 +1,8 @@
 import os
 
+version = "0.2.3"
+
+
 # Env var CI is set by Travis build process,
 # see: https://docs.travis-ci.com/user/environment-variables/#Default-Environment-Variables
 is_ci_build = os.getenv("CI", 0)
@@ -7,6 +10,6 @@ is_ci_build = os.getenv("CI", 0)
 if is_ci_build:
     dev = ""
 else:
-    dev = "-dev"
-version = "0.2.0"
+    dev = ""
+
 build_string = "{}{}".format(version, dev)
--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+if [ -n "$(git status --porcelain)" ]; then
+    git status
+    echo "*** There are changes, aborting release, update version.py and commit before retrying ***";
+    exit
+fi
+
 V=$(python3 setup.py --version)
 DESC=$V
-git tag -a $V -m $DESC
-git push
-git push --tag
+git tag -a $V -m $DESC && git push && git push --tag
